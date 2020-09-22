3acf960d2f91b5254b9098fd2629ce08383f3554
Nobuhito Nishihara <a01252111@yahoo.co.jp>
2020-09-22 12:28:39 +0900

    ２つ目のテスト課題を実装

--- 02_object_model/hoge.rb
+++ 02_object_model/hoge.rb
@@ -1,7 +1,14 @@
-class Hoge
+class Hoge < String
   # Hogeクラスの仕様
   # "hoge" という文字列の定数Hogeを持つ
+  Hoge = "hoge"
   # "hoge" という文字列を返すhogehogeメソッドを持つ
+  def hogehoge
+    return Hoge
+  end
   # HogeクラスのスーパークラスはStringである
   # 自身が"hoge"という文字列である時（HogeクラスはStringがスーパークラスなので、当然自身は文字列である）、trueを返すhoge?メソッドが定義されている
+  def hoge?
+    return self === Hoge
+  end
 end
--- 02_object_model/hoge_everywhere.rb
+++ 02_object_model/hoge_everywhere.rb
@@ -1,8 +1,38 @@
 # 次に挙げるクラスのいかなるインスタンスからも、hogeメソッドが呼び出せる
 # それらのhogeメソッドは、全て"hoge"という文字列を返す
 # - String
+class String
+  def hoge
+    "hoge"
+  end
+end
 # - Integer
+class Integer
+  def hoge
+    "hoge"
+  end
+end
 # - Numeric
+class Numeric
+  def hoge
+    "hoge"
+  end
+end
 # - Class
+class Class
+  def hoge
+    "hoge"
+  end
+end
 # - Hash
+class Hash
+  def hoge
+    "hoge"
+  end
+end
 # - TrueClass
+class TrueClass
+  def hoge
+    "hoge"
+  end
+end
